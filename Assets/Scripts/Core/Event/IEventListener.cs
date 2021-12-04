d91832b26b3296c84f95537a2bd8e798311efc05
allen biehl <allenbiehl@hotmail.com>
2021-12-04 09:20:16 -0500

    Add core documentation

--- Assets/Scripts/Core/Event/IEventListener.cs
+++
@@ -1,7 +0,0 @@
-namespace GameHub.Core.Event
-{
-    public interface IEventListener<T>
-    {
-        public void OnEvent( T eventType );
-    }
-}
--- Assets/Scripts/Core/Security/UserInfoManager.cs
+++ Assets/Scripts/Core/Security/UserInfoManager.cs
@@ -1,10 +1,27 @@
 namespace GameHub.Core.Security
 {
+    /// <summary>
+    /// Class <c>UserInfoManager</c> provides the ability to load <c>UserInfo</c> 
+    /// associated with the current user as well as saving user information
+    /// across gaming sessions.
+    /// </summary>
     public class UserInfoManager
     {
+        /// <summary>
+        /// Instance variable <c>_cachedUserInfo</c> stores the cached <c>UserInfo</c> 
+        /// instance. 
+        /// </summary>
         private static UserInfo _cachedUserInfo;
 
-
+        /// <summary>
+        /// Method <c>SaveUserInfo</c> is used to serialize the <c>UserInfo</c> and
+        /// write the associated data to the underlying storage mechanism, which may 
+        /// be the file system or a database.
+        /// </summary>
+        /// <param name="userInfo">
+        /// <c>userInfo</c> represents the <c>UserInfo</c> to save / write to the 
+        /// underylying storage mechanism.
+        /// </param>
         public static void SaveUserInfo(UserInfo userInfo)
         {
             GameSettingsManager.SaveSettings("UserSettings", userInfo);
@@ -12,11 +29,26 @@ namespace GameHub.Core.Security
             _cachedUserInfo = userInfo;
         }
 
+        /// <summary>
+        /// Method <c>LoadUserInfo</c> is used to retrieve the <c>UserInfo</c> associated
+        /// with the current user directly from the underyling storage mechanism, bypassing
+        /// any cached reference. Typically you should call <c>GetUserInfo</c> to speed
+        /// up retrieval of the cached <c>UserInfo</c>.
+        /// </summary>
+        /// <returns>
+        /// Returns instance of <c>UserInfo</c> associated with the current user.
+        /// </returns>
         public static UserInfo LoadUserInfo()
         {
             return GameSettingsManager.LoadSettings<UserInfo>("UserSettings");
         }
 
+        /// <summary>
+        /// Method <c>GetUserInfo</c> is used to retrieve a) current cached <c>UserInfo</c>
+        /// instance, persisted <c>UserInfo</c> instance if available in the underlying
+        /// storage mechanism, or a generic <c>UserInfo</c> placeholder. 
+        /// </summary>
+        /// <returns></returns>
         public static UserInfo GetUserInfo()
         {
             if (_cachedUserInfo != null)
