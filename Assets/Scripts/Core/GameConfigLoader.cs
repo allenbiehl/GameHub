1827b6dc855abc45cbe84a0455f6d5e0a6c0f722
allen biehl <allenbiehl@hotmail.com>
2021-12-04 18:35:17 -0500

    Clean up game panel references and callbacks

--- Assets/Scripts/Core/GameConfigLoader.cs
+++ Assets/Scripts/Core/GameConfigLoader.cs
@@ -1,12 +1,49 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using GameHub.Core.Util;
 
 namespace GameHub.Core
 {
-    public static class GameConfigLoader
+    /// <summary>
+    /// Class <c>GameConfigLoader</c> provides the ability to retrieve a list of 
+    /// all available game configurations that can be played.
+    /// </summary>
+    public class GameConfigLoader
     {
-        public static List<GameConfig> Load()
+        /// <summary>
+        /// Instance variable <c>_instance</c> stores the <c>GameConfigLoader</c>
+        /// singleton instance.
+        /// </summary>
+        private static readonly Lazy<GameConfigLoader> _instance =
+            new Lazy<GameConfigLoader>(() => new GameConfigLoader());
+
+        /// <summary>
+        /// Method <c>Instance</c> returns the <c>GameConfigLoader</c> singleton 
+        /// instance.
+        /// </summary>
+        public static GameConfigLoader Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        /// <summary>
+        /// Private constructor to ensure the <c>GameConfigLoader</c> cannot 
+        /// be instantiated externally.
+        /// </summary>
+        private GameConfigLoader()
+        {
+        }
+
+        /// <summary>
+        /// Method <c>Load</c> is used to retrieve a list of all available game 
+        /// configurations that can be played.
+        /// </summary>
+        /// <returns></returns>
+        public List<GameConfig> Load()
         {
             Sprite sprite;
             List<GameConfig> gameConfigs = new List<GameConfig>();
